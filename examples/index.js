4c2edc96655033992dfb9605f8a81a11db21bcf3
Planeshifter <pburckhardt@outlook.com>
2015-05-23 00:26:58 -0400

    [UPDATE] implementation

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,15 @@
 'use strict';
 
-var foo = require( './../lib' );
+var distance = require( './../lib' );
+
+var x = new Array( 100 ),
+	y = new Array( 100 ),
+	d;
+
+for ( var i = 0; i < x.length; i++ ) {
+	x[ i ] = Math.round( Math.random()*100 );
+	y[ i ] = Math.round( Math.random()*100 );
+}
+d = distance( x, y );
+
+console.log( d );
